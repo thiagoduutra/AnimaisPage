f0aa679c8205936ea97952b5fd841aeb97d0833c
ThiagoDuutra <thiagoduutra01@gmail.com>
2022-12-02 19:13:44 -0300

    feat(#8): finalizado projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,64 @@
 const tabImgs = document.querySelectorAll(".js-nav li");
+
 const tabDesc = document.querySelectorAll(".js-navDescription div");
 
 const links = document.querySelectorAll('.js-menu a[href^="#"]');
-if (tabImgs.length && tabDesc.length) {
-    tabDesc[0].classList.add("active");
 
-    function activeTab(index) {
-        tabDesc.forEach((div) => {
-            div.classList.remove("active");
+function scrollNav() {
+    if (tabImgs.length && tabDesc.length) {
+        tabDesc[0].classList.add("active");
+
+        function activeTab(index) {
+            tabDesc.forEach((div) => {
+                div.classList.remove("active");
+            });
+            tabDesc[index].classList.add("active");
+        }
+
+        tabImgs.forEach((item, index) => {
+            item.addEventListener("click", () => {
+                activeTab(index);
+            });
         });
-        tabDesc[index].classList.add("active");
     }
 
-    tabImgs.forEach((item, index) => {
-        item.addEventListener("click", () => {
-            activeTab(index);
+    function scrollToDiv(e) {
+        e.preventDefault();
+        const href = e.currentTarget.getAttribute("href");
+        const div = document.querySelector(href);
+
+        div.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
         });
+    }
+
+    links.forEach((link) => {
+        link.addEventListener("click", scrollToDiv);
     });
 }
+scrollNav();
 
-function scrollToDiv(e) {
-    e.preventDefault();
-    const href = e.currentTarget.getAttribute("href");
-    const div = document.querySelector(href);
+function initAnimation() {
+    const containerScroll = document.querySelectorAll(".js-scroll");
 
-    div.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-    });
+    if (containerScroll.length) {
+        const windowHalf = window.innerHeight * 0.6;
+
+        function animationScroll() {
+            containerScroll.forEach((divs) => {
+                //
+                const divTop = divs.getBoundingClientRect().top;
+                const divVisible = divTop - windowHalf < 0;
+                if (divVisible) {
+                    divs.classList.add("active");
+                } else {
+                    divs.classList.remove("active");
+                }
+            });
+        }
+        animationScroll();
+        window.addEventListener("scroll", animationScroll);
+    }
 }
-
-links.forEach((link) => {
-    link.addEventListener("click", scrollToDiv);
-});
+initAnimation();
